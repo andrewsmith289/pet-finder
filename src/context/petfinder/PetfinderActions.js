1d3eccfe17b22966073c30f58b0a9cbca0062bd5
Andrew Smith <andrew.c.smith289@gmail.com>
2022-06-06 16:24:07 -0400

    implemented axios interceptor + token refresher

--- src/context/petfinder/PetfinderActions.js
+++ src/context/petfinder/PetfinderActions.js
@@ -1,13 +1,41 @@
 import axios from 'axios'
 
 const PETFINDER_URL = process.env.REACT_APP_PETFINDER_URL
-const PETFINDER_TOKEN = process.env.REACT_APP_PETFINDER_TOKEN
+const PETFINDER_CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const PETFINDER_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
-console.log(PETFINDER_TOKEN)
 const petfinder = axios.create({
   baseURL: PETFINDER_URL,
-  headers: { Authorization: `Bearer ${PETFINDER_TOKEN}` },
 })
+
+petfinder.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async function (error) {
+    const originalRequest = error.config
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      const access_token = await refreshAccessToken()
+      petfinder.defaults.headers.common['Authorization'] =
+        'Bearer ' + access_token
+
+      return petfinder(originalRequest)
+    }
+
+    return Promise.reject(error)
+  }
+)
+
+const refreshAccessToken = async () => {
+  const data = await petfinder.post(`oauth2/token`, {
+    grant_type: 'client_credentials',
+    client_id: PETFINDER_CLIENT_ID,
+    client_secret: PETFINDER_CLIENT_SECRET,
+  })
+
+  return data.data.access_token
+}
 
 export const searchPets = async (text) => {
   const params = new URLSearchParams({
