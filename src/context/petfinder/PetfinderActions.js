3dfadc1ade56efb3c4cebbd7aaf987e9a50c9dbb
Andrew Smith <andrew.c.smith289@gmail.com>
2022-06-07 21:09:21 -0400

    Migrated to proxy api to get around cors.

--- src/context/petfinder/PetfinderActions.js
+++ src/context/petfinder/PetfinderActions.js
@@ -1,48 +1,20 @@
 import axios from 'axios'
 
 const PETFINDER_URL = process.env.REACT_APP_PETFINDER_URL
-const PETFINDER_CLIENT_ID = process.env.REACT_APP_CLIENT_ID
-const PETFINDER_CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
 
 const petfinder = axios.create({
   baseURL: PETFINDER_URL,
-})
-
-petfinder.interceptors.response.use(
-  (response) => {
-    return response
+  headers: {
+    'Content-Type': 'application/json',
   },
-  async function (error) {
-    const originalRequest = error.config
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true
-      const access_token = await refreshAccessToken()
-      petfinder.defaults.headers.common['Authorization'] =
-        'Bearer ' + access_token
-
-      return petfinder(originalRequest)
-    }
-
-    return Promise.reject(error)
-  }
-)
-
-const refreshAccessToken = async () => {
-  const data = await petfinder.post(`oauth2/token`, {
-    grant_type: 'client_credentials',
-    client_id: PETFINDER_CLIENT_ID,
-    client_secret: PETFINDER_CLIENT_SECRET,
-  })
-
-  return data.data.access_token
-}
+})
 
 export const searchPets = async (text) => {
   const params = new URLSearchParams({
     name: text,
   })
 
-  const res = await petfinder.get(`animals?${params}`)
-
+  const res = await petfinder.get(`pets?${params}`)
+  console.log(res)
   return res.data.animals
 }
